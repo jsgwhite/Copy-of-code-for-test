20d156008d60363f1bcd31f07503475f64365ed9
jsgwhite <jsgwhite@gmail.com>
2017-02-06 22:56:30 +1300

    Jason copy of TpcInterviewTest code

---
+++ Services/Logger.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IMessageLogger
+    {
+        void Log(string message);
+    }
+
+    public class ConsoleLogger : IMessageLogger
+    {
+        public void Log(string message)
+        {
+            // Do some work here.
+            Console.WriteLine(message);
+        }
+    }
+
+    public class QueueLogger : IMessageLogger
+    {
+        public void Log(string message)
+        {
+            // Do some work here.
+        }
+    }
+
+
+    /// <summary>
+    /// This class is designed to perform a basic logging function.
+    /// In the future, we might want to expand our logging capabilities. 
+    /// 
+    /// Currently, this class doesn't follow SOLID and would require too much modification to extext. 
+    /// It violates the Open Close Principle. 
+    /// 
+    /// Please refactor this method and provide tests in Services.UnitTests.LoggerTests.LogShould.
+    /// 
+    /// 
+    /// Hint:
+    /// 
+    /// public class ConsoleLogger : IMessageLogger {}
+    /// public class QueueLogger : IMessageLogger {}
+    /// </summary>
+    public class Logger
+    {
+        public IMessageLogger Logr{ get; set; }
+
+        public Logger(IMessageLogger byoLogr)
+        {
+            Logr = byoLogr;
+        }
+
+        public Logger(LogType logType)
+        {
+            switch (logType)
+            {
+                case LogType.Console:
+                    Logr = new ConsoleLogger();
+                    break;
+
+                case LogType.Queue:
+                    Logr = new QueueLogger();
+                    break;
+            }
+        }
+
+        public void Log(string message)
+        {
+            Logr.Log(message);
+            
+        }
+    }
+
+
+    public enum LogType
+    {
+        Console,
+        Queue
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,110 @@
+﻿using Moq;
+using Services;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace UnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void ConsoleLogger_Exists()
+        {
+            var logr = new ConsoleLogger();
+            logr.Log("Test msg.");
+
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void QueueLogger_Exists()
+        {
+            var logr = new QueueLogger();
+            logr.Log("Test msg.");
+
+            Assert.True(true);
+        }
+        
+        [Fact]
+        public void QueueLogger_Implements_LogMethod()
+        {
+            IMessageLogger emptyLogr;
+
+            var logr = new QueueLogger();
+            logr.Log("Test msg.");
+
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void QueueLogger_DerivesFromILogger()
+        {
+            IMessageLogger emptyLogr;
+
+            var logr = new QueueLogger();
+            logr.Log("Test msg.");
+
+            Assert.True(logr is IMessageLogger);
+        }
+
+        [Fact]
+        public void ConsoleLogger_DerivesFromILogger()
+        {
+            IMessageLogger emptyLogr;
+
+            var logr = new ConsoleLogger();
+            logr.Log("Test msg.");
+
+            Assert.True(logr is IMessageLogger);
+        }
+
+        [Fact]
+        public void Logger_TakesLogTypeInCtor()
+        {
+            var logger = new Logger(LogType.Console);
+            
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Logger_TakesInjectedLoggerInCtor()
+        {
+            var logr = new ConsoleLogger();
+            var logger = new Logger(logr);
+
+            Assert.True(true);
+        }
+
+
+
+        // --- Moq tests ---
+        
+        [Fact]
+        public void WhenInjectSpecificLoggerToCtor_VerifyLogrPropHasInjectedObject()
+        {
+            Mock<ConsoleLogger> mockConsole = new Mock<ConsoleLogger>();
+            //Mock<Logger> mockLogger = new Mock<Logger>();
+            var logger = new Logger(mockConsole.Object);
+
+            //mockLogger.Verify(t => t.Log("test"));
+            Assert.Equal(logger.Logr, mockConsole.Object);
+        }
+
+        [Fact]
+        public void WhenPassingInConsoleLogTypeToCtor_VerifyLogrPropHasCorrectLoggerType()
+        {
+            Mock<Logger> mockLogger = new Mock<Logger>(LogType.Console);
+            
+            Assert.True(mockLogger.Object.Logr is ConsoleLogger);
+        }
+
+        [Fact]
+        public void WhenPassingInQueueLogTypeToCtor_VerifyLogrPropHasCorrectLoggerType()
+        {
+            Mock<Logger> mockLogger = new Mock<Logger>(LogType.Queue);
+
+            Assert.True(mockLogger.Object.Logr is QueueLogger);
+        }
+    }
+}
